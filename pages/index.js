a33d97bba3195583308312c8c5622c0b093cb7ae
Hernandes Falcão <39099119+HernandesFalcao@users.noreply.github.com>
2023-04-26 01:47:46 +0000

    feat: add index page

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+function Home() {
+    return (
+        <myMessage>
+            <h1>✨Você é incrível!✨</h1>
+            <h2>❤️Não importa o que os outros pensam ou digam sobre você❤️</h2>
+            <h3>Tenha orgulho de si mesmo!</h3>
+        </myMessage>
+    );
+}
+
+export default Home;
